593d682061a74b4c8534b687836ce40c259d6c9c
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-09 09:04:57 -0700

    pass test: getCost() returns cost

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,20 @@ function Pizza(size, toppings){
   this.toppings=toppings;
 }
 
+Pizza.prototype.getCost=function(){
+  let cost=0;
+  if(this.size === "small") {
+    cost+=10;
+  }
+  if(this.toppings === "cheese") {
+    cost+=0;
+  }
+  return cost;
+}
+
 //TESTS
 let myPizza = new Pizza("small", "cheese");
 console.log("You ordered a small cheese pizza");
 console.log(`You ordered a ${myPizza.size} ${myPizza.toppings} pizza`);
+console.log(10);
+console.log(myPizza.getCost());
