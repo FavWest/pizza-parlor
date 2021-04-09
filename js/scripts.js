dd65ae9f025dbc4bc260d15bb496f59f53bcb2ba
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-09 09:12:20 -0700

    pass test: cost changes based on Pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,18 @@ function Pizza(size, toppings){
 
 Pizza.prototype.getCost=function(){
   let cost=0;
-  if(this.size === "small") {
-    cost+=10;
+  switch(this.size) {
+    case ("small"):
+      cost+=10;
+      break;
+    case ("medium"):
+      cost+=15;
+      break;
+    case ("large"):
+      cost+=20;
+      break;
+    default:
+      cost="error, cannot calculate cost; invalid response received for pizza size. Please try again."
   }
   if(this.toppings === "cheese") {
     cost+=0;
@@ -19,4 +29,7 @@ let myPizza = new Pizza("small", "cheese");
 console.log("You ordered a small cheese pizza");
 console.log(`You ordered a ${myPizza.size} ${myPizza.toppings} pizza`);
 console.log(10);
+console.log(myPizza.getCost());
+myPizza = new Pizza("large", "cheese");
+console.log(20)
 console.log(myPizza.getCost());
